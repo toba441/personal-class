6cc1fd382b6c2bf0ce837a072ac45738d89b2a43
Dannyfsp <femoscopilala@gmail.com>
2024-03-27 13:16:29 +0100

    lesson on variables

--- variable.js
+++ variable.js
@@ -1,1 +1,38 @@
-let varName = "I'm excited to be learning javascript"
+// VARIABLES
+// A variable is like a container where data or information is stored
+// It can also be seen as a box that holds info of data types or functions
+
+// 2 types of variables
+// mutable variable: means that the info inside the variable can change E.g variables with let or var
+// immutable variable: means that the info inside the variable cannot be changed E.g variables with const
+
+let varName = "I'm excited" // mutable
+
+varNo = 456
+
+console.log(varName);
+
+
+
+// For variables, you need to understand the terms: declaring and assignment
+// for declaring you either use var, let or const
+// for assignment you use "="
+
+let lastName; // declaration
+
+// For constants variables, you need to declare and assign immediately, declaration alone does not work.
+
+
+lastName = "Isiaih" // assignment
+
+
+const firstName = "Daniel" // declare and assign to an immutable variable
+
+console.log(firstName + " " + lastName);
+
+
+// ASSIGMENT
+// 1. Declare a mutable variable
+// 2. Assign the mutable variable you declared to save your fav color
+// 3. Declare and assign an immutable variable to save your nickname
+// 4. Print to the console or terminal your saved fav color and nickname.
